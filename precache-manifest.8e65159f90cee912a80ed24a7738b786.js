337435f3ea24bb84ad386fb7b3a6bae311055723
Felix Höhne <felix.hoehne@akelius.de>
2019-02-05 15:26:12 +0100

    Updates

---
+++ precache-manifest.8e65159f90cee912a80ed24a7738b786.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e836aec6d8ddefc3fc12",
+    "url": "/Places/static/js/runtime~main.e836aec6.js"
+  },
+  {
+    "revision": "3bfa612d753860381ff0",
+    "url": "/Places/static/js/main.3bfa612d.chunk.js"
+  },
+  {
+    "revision": "73e90a27f1e00d8abe47",
+    "url": "/Places/static/js/1.73e90a27.chunk.js"
+  },
+  {
+    "revision": "c07028e7768ded21405ec4256dc8097f",
+    "url": "/Places/index.html"
+  }
+];
